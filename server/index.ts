b5a0232b756865764aed42f905eeeec43e485f3f
Lucas <lucasjoao85@gmail.com>
2020-11-27 14:46:28 -0300

    Removendo Menu

--- server/index.ts
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const Api = axios.create({
-    baseURL: "https://personal-blog-lucas-api.herokuapp.com"
-})
-
-export default Api;
--- util/Constants.ts
+++
@@ -1,6 +0,0 @@
-export default module.exports = {
-    TIME_TRANSITION: 0.3,
-    API: {
-        base_url: 'https://personal-blog-lucas-api.herokuapp.com'
-    }
-}
